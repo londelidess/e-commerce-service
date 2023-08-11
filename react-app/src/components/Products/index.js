448ccf9ef618fc616fe54b36f571834a777a3654
makoto <d.makoto0701@yahoo.com>
2023-08-11 13:58:20 -0700

    env.py

---
+++ react-app/src/components/Products/index.js
@@ -0,0 +1,12 @@
+// ProductLandingPage.js
+import React from 'react';
+
+function ProductLandingPage() {
+  return (
+    <div>
+      <h2>Products</h2>
+    </div>
+  );
+}
+
+export default ProductLandingPage;
