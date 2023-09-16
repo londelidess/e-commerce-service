4e83ed1b19d705da91a390afd4b8842114633dff
makoto <d.makoto0701@yahoo.com>
2023-09-16 03:03:31 -0700

    nap

--- react-app/src/components/HomePage/Contact.js
+++ react-app/src/components/HomePage/Contact.js
@@ -1,8 +1,23 @@
 import React from "react";
 import logo from "../../images/playbox-high-resolution-logo-black-on-transparent-background.png";
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import "./Contact.css";
 
 function Contact() {
+  const containerStyle = {
+    width: '100%',
+    height: '400px'
+  };
+
+  const center = {
+    lat: 33.7594,
+    lng: -118.0043
+  };
+
+  const markerPosition = {
+    lat: 33.7594,
+    lng: -118.0043
+  };
   return (
     <div className="contact-page">
       <h1 className="contact-title">Contact Us</h1>
@@ -16,18 +31,18 @@ function Contact() {
         action="https://formspree.io/f/meqbkwey"
         method="POST"
       >
-<div className="form-group">
-    <div className="name-group">
-        <div className="single-name">
-            <label htmlFor="firstName">First Name (required)</label>
-            <input type="text" id="firstName" name="firstName" required />
+        <div className="form-group">
+            <div className="name-group">
+                <div className="single-name">
+                    <label htmlFor="firstName">First Name (required)</label>
+                    <input type="text" id="firstName" name="firstName" required />
+                </div>
+                <div className="single-name">
+                    <label htmlFor="lastName">Last Name (required)</label>
+                    <input type="text" id="lastName" name="lastName" required />
+                </div>
+            </div>
         </div>
-        <div className="single-name">
-            <label htmlFor="lastName">Last Name (required)</label>
-            <input type="text" id="lastName" name="lastName" required />
-        </div>
-    </div>
-</div>
         <div className="form-group">
           <label htmlFor="email">Email Address (required)</label>
           <input type="email" id="email" name="email" required />
@@ -53,6 +68,20 @@ function Contact() {
           <br />
           United States
         </p>
+
+        <LoadScript
+          googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+        >
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={10}
+            mapId="8e0a97af9386fef"
+          >
+            <Marker position={markerPosition} title="8622 Crockett Cir. Westminster, CA 92683, United States" />
+
+          </GoogleMap>
+        </LoadScript>
       </div>
     </div>
   );
