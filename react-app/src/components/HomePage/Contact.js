7fc630632dc7c1a58d42ae0ef5a9bf6723540a63
makoto <d.makoto0701@yahoo.com>
2023-09-16 04:49:32 -0700

    google map finished

--- react-app/src/components/HomePage/Contact.js
+++ react-app/src/components/HomePage/Contact.js
@@ -1,23 +1,76 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import logo from "../../images/playbox-high-resolution-logo-black-on-transparent-background.png";
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker, StandaloneSearchBox, InfoWindow   } from '@react-google-maps/api';
 import "./Contact.css";
 
+const containerStyle = {
+  width: '100%',
+  height: '400px'
+};
+
+const center = {
+  lat: 34.125499909512676,
+  lng: -118.07488384954036,
+};
+
+const markerPosition = {
+  lat: 34.125499909512676,
+  lng: -118.07488384954036,
+};
+
+const libraries = ["places"];
+
+const mapStyles =
+[
+  {
+      "stylers": [
+          {
+              "saturation": 100
+          },
+          {
+              "gamma": 0.6
+          }
+      ]
+  }
+]
+
 function Contact() {
-  const containerStyle = {
-    width: '100%',
-    height: '400px'
+
+  const [searchBox, setSearchBox] = useState(null);
+  const [mapCenter, setMapCenter] = useState(center);
+  const [showInfoWindow, setShowInfoWindow] = useState(false);
+
+  const onPlacesChanged = () => {
+    if (!searchBox) return;
+
+    const places = searchBox.getPlaces();
+
+    if (!places || places.length === 0) return;
+
+    const bounds = new window.google.maps.LatLngBounds();
+
+    places.forEach(place => {
+      if (!place.geometry) return;
+
+      if (place.geometry.viewport) {
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+
+    const firstPlace = places[0];
+    if (firstPlace.geometry && firstPlace.geometry.location) {
+      const nextCenter = firstPlace.geometry.location;
+      setMapCenter({ lat: nextCenter.lat(), lng: nextCenter.lng() });
+    }
   };
 
-  const center = {
-    lat: 33.7594,
-    lng: -118.0043
+  const onSearchBoxLoaded = (ref) => {
+    setSearchBox(ref);
   };
 
-  const markerPosition = {
-    lat: 33.7594,
-    lng: -118.0043
-  };
+
   return (
     <div className="contact-page">
       <h1 className="contact-title">Contact Us</h1>
@@ -62,24 +115,62 @@ function Contact() {
       <div className="contact-footer">
         <img className="logo-in-contact" src={logo} alt="Home" />
         <p className="address-in-contact">
-          8622 Crockett Cir.
+          8913 E Greenwood Ave,
           <br />
-          Westminster, CA 92683
+          San Gabriel, CA 91775
           <br />
           United States
         </p>
 
         <LoadScript
-          googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+        googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+        libraries={["places"]}
+      >
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={mapCenter}
+          zoom={11}
+          options={{ styles: mapStyles }}
         >
-          <GoogleMap
-            mapContainerStyle={containerStyle}
-            center={center}
-            zoom={10}
-            mapId="8e0a97af9386fef"
-          >
-            <Marker position={markerPosition} title="8622 Crockett Cir. Westminster, CA 92683, United States" />
 
+        <Marker
+        position={markerPosition}
+        onMouseOver={() => setShowInfoWindow(true)}
+        onMouseOut={() => setShowInfoWindow(false)}
+      >
+        {showInfoWindow && (
+          <InfoWindow onCloseClick={() => setShowInfoWindow(false)}>
+            <div>PLAYBOX</div>
+          </InfoWindow>
+        )}
+      </Marker>
+          <StandaloneSearchBox
+            onLoad={onSearchBoxLoaded}
+            onPlacesChanged={onPlacesChanged}
+          >
+
+          <input
+            type="text"
+            placeholder="Search location"
+            style={{
+              boxSizing: `border-box`,
+              border: `1px solid transparent`,
+              width: `240px`,
+              height: `32px`,
+              marginTop: `10px`,
+              padding: `0 12px`,
+              borderRadius: `3px`,
+              boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+              fontSize: `14px`,
+              outline: `none`,
+              textOverflow: `ellipses`,
+              position: "absolute",
+              top: "10px",
+              left: "50%",
+              transform: "translateX(-50%)"
+            }}
+          />
+        </StandaloneSearchBox>
           </GoogleMap>
         </LoadScript>
       </div>
