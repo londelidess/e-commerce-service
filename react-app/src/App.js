4501423ba28da6fdff10cdce5d7fac958e387326
makoto <d.makoto0701@yahoo.com>
2023-09-16 14:55:36 -0700

    chat bot

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -19,7 +19,7 @@ import ProductManage from "./components/Products/ProductManage"
 import ProductShow from "./components/Products/ProductShow";
 import Favorite from "./components/Favorite";
 import Reviews from "./components/Reviews";
-
+import ChatBot from './components/ChatBot';
 
 
 // import PrivateRoute from "./PrivateRoute"
@@ -59,6 +59,7 @@ function App() {
           <Route exact path="/" component={HomePage} />
         </Switch>
       )}
+      <ChatBot />
       <Footer />
     </>
   );
---
+++ react-app/src/components/ChatBot/index.js
@@ -0,0 +1,19 @@
+// components/ChatBot.js
+import React, { useState } from 'react';
+import { IoSend } from "react-icons/io5";
+import BeatLoader from "react-spinners/BeatLoader";
+import * as openai from "openai";
+import "./ChatBot.css";
+console.log(openai);
+
+function ChatBot() {
+
+
+    return (
+        <div className="chatbot-container">
+
+        </div>
+    );
+}
+
+export default ChatBot
